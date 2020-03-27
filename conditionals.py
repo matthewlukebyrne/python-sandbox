58298f06b9fa0f4aa55ae8d04940aa641a39aac4
Matthew Byrne <matthewlukebyrne@gmail.com>
2020-03-27 18:46:07 +0000

    Conditions, Functions

--- conditionals.py
+++ conditionals.py
@@ -1,58 +1,67 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
 x = 10
-y = 1
+y = 10
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
+# Standard Stuff
 
 # Simple if
-if x == y:
-  print(f'{x} is equal to {y}')
+# Use of F-String (format examples)
+# Will return nothing to console if not true
+# if x == y:
+#   print(f'{x} is equal to {y}')
 
-# If/else
+# # If/else
 # if x > y:
 #   print(f'{x} is greater than {y}')
 # else:
 #   print(f'{x} is less than {y}')
+
+
+
   
 # elif
 # if x > y:
 #   print(f'{x} is greater than {y}')
+# Else if operators (other options)
 # elif x == y:
 #   print(f'{x} is equal to {y}')  
 # else:
 #   print(f'{x} is less than {y}')
 
-# Nested if
+
+
+# Nested if (Not used much better option below with logical operators)
 # if x > 2:
-#   if x <= 10:
+#   if x <= 11:
 #     print(f'{x} is less than 2 and greater than 10')
     
 
 # Logical operators (and, or, not) - Used to combine conditional statements
 
-# and
+# and (Both have to)
 # if x > 2 and x <=10:
 #   print(f'{x} is less than 2 and greater than 10')
 
-# or
+# # or (Only one has to match)
 # if x > 2 or x <=10:
 #   print(f'{x} is less than 2 or greater than 10')
 
-# not
+# # not 
 # if not(x == y):
 #   print(f'{x} is not equal to {y}')
 
 
 # Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
 
-numbers = [1,2,3,4,5]
+# numbers = [1,2,3,4,5]
 
-# in
+# in (Returns nothing if false)
 # if x in numbers:
 #   print(x in numbers)
 
-# in
+# not in
 # if x not in numbers:
 #   print(x in numbers)
 
@@ -65,5 +74,5 @@ numbers = [1,2,3,4,5]
 
 
 # is not
-if x is not y:
-  print(x is not y)
+# if x is not y:
+#   print(x is not y)
--- functions.py
+++ functions.py
@@ -1,32 +1,52 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 # Create function
-def sayHello(name = 'Sam'):
+def sayHello(name):
   """
-  Prints Hello and then name.
+  Prints Hello and then Name (This is a docstring)
   """
   print('Hello ' + name)
+# Call and indent out
+sayHello('Sam')
 
-# Return value
+
+
+# Return Value
 def getSum(num1, num2):
+  """
+  Definition of scope also valid in python functions
+  """
   total = num1 + num2
   return total
+# Indent out and print
+numSum = getSum(2,3)
+print(numSum)
 
-numSum = getSum(2, 3)
 
+
+# Another Function value
 def addOneToNum(num):
-  num += 1
+  """
+  sets up the +2 and then adds outside indentation
+  to equal 8
+  """
+  num += 2
   return num
-
-num = 5
+num = 6
 new_num = addOneToNum(num)
 print(new_num)
+
+
+
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
-getSum = lambda num1, num2 : num1 + num2
-print(getSum(9, 2))
 
+# Dont need a return
+getSum = lambda num1, num2 : num1 + num2
+print(getSum(9 , 2))
+
+# Totals 7
 addOneToNum = lambda num : num + 1
-print(addOneToNum(5))
+print(addOneToNum(6))
